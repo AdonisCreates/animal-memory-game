a4977fe62fe732d6a55ac237b91e44418779991a
Adriel Adonis <arodriguez32@fisher.edu>
2020-08-01 11:28:50 -0400

    added css to cards to display them in the middle and rotate 180deg

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log($);
+// list of all cards store in memory
+const cards = document.querySelectorAll('.memory-card')
+
+function flipCard() {
+    this.classList.toggle('flip');
+    console.log("I've been clicked!");
+    // toggle means if the class is there, remove it, if it is there, add it.
+}
+
+// event listener
+cards.forEach(card => card.addEventListener('click', flipCard))
