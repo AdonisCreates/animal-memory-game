0cd7d65bc22a30a20d77f133c1ee7a06e3497ef5
Adriel Adonis <arodriguez32@fisher.edu>
2020-08-03 21:15:56 -0400

    fixed images to match game logic and started js logic

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 // list of all cards store in memory
 const cards = document.querySelectorAll('.memory-card')
 
+// when player flips card, we have to know if it's the first or second card, below is the logic for it
+let hasBeenFlipped = false;
+let firstCard, secondCard;
+
 // this will loop through my list
 function flipCard() {
     this.classList.toggle('flip');
-    console.log("I've been clicked!");
     // toggle means if the class is there, remove it, if it is there, add it.
+    if (!hasBeenFlipped) {
+        // first click of the card
+        hasBeenFlipped = true;
+        firstCard = this;
+    } else {
+        // second click of the card
+        hasBeenFlipped = false;
+        secondCard = this;
+
+        console.log({firstCard, secondCard});
+    }
 }
 
 // event listener
